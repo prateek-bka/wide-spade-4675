f849896dc6ddcfdc1ea578215487ede712065275
prateek-bka <prateek.bka@gmail.com>
2022-11-10 18:07:25 +0530

    added day-3

---
+++ src/Components/Slider.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+
+const Slider = () => {
+  return (
+    <>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://files.myglamm.com/site-images/original/2240x614-Gifting-Store-KV-refresh.jpg"
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://files.myglamm.com/site-images/original/Homepage-1920x527_3.jpeg"
+            alt="Second slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://files.myglamm.com/site-images/original/Homepage-desktop---1920-x-527.png"
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://files.myglamm.com/site-images/original/popxo1920x527.gif"
+            alt="Fourth slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://files.myglamm.com/site-images/original/Manish1920x527.jpg"
+            alt="Fifth slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+      <div className="">
+        <img
+          src="https://files.myglamm.com/site-images/original/ULt2240x614.jpg"
+          alt=""
+        />
+      </div>
+    </>
+  );
+};
+
+export default Slider;
---
+++ src/Container/Products/ProductContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductContainer = () => {
+  return <div>ProductContainer</div>;
+};
+
+export default ProductContainer;
