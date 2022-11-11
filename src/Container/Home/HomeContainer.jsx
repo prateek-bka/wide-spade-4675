4e8f1a77d5c609d1524555ec6dfb40e3f040ed71
prateek-bka <prateek.bka@gmail.com>
2022-11-11 21:37:58 +0530

    added day-4

--- src/Container/Home/HomeContainer.jsx
+++ src/Container/Home/HomeContainer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "../../Components/Footer";
 
 import Slider from "../../Components/Slider";
 import SpotlightHome from "../../Components/SpotlightHome";
@@ -8,6 +9,7 @@ const HomeContainer = () => {
     <>
       <Slider />
       <SpotlightHome />
+      {/* <Footer /> */}
     </>
   );
 };
